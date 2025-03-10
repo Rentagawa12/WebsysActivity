1ca78489b849dca7ef54c2ff1261f208fccaf76f
Rentagawa <miguelangelocabrera1@gmail.com>
2025-03-10 22:34:20 +0800

    Second Commit

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
 
     path('<int:post_id>/share/',
      views.post_share, name='post_share'),
+
+     path('tag/<slug:tag_slug>',
+          views.post_list, name='post_list_by_tag'),
 ]
 
